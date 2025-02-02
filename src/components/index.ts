907a0ab15d8664dc924555be6b747c23eaf5e18c
UsamaNawaz <usamanawaz659@gmail.com>
2025-02-02 22:20:04 +0500

    chore: tsconfig and fix type conversion issue in Firebase timestamp

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,3 @@ export * from './Input/Input';
 export * from './Btn/index';
 export * from './AvatarInitial/AvatarInitial';
 export * from './AnimatedBtn/AnimatedBtn';
-export * from './FadeText/FadeText';
